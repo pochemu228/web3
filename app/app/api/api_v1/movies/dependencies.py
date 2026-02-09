9b1351c7bdecb8d21735a492edfd933da1ae69f4
арь <arinasmirnova096@gmail.com>
2026-02-09 19:50:20 +0900

    dependencies.py

--- app/app/api/api_v1/movies/dependencies.py
+++ app/app/api/api_v1/movies/dependencies.py
@@ -1,7 +1,8 @@
 from fastapi import HTTPException, status
-from LB3.modules.movie import Movie
+from schemas.movie import Movie
 from .crud import movie_storage
 
 
 def get_movie_by_slug(slug: str) -> Movie:
+
     return movie_storage.get_by_slug(slug)
